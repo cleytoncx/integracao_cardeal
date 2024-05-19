70801b89d429f378f955f331348fd3f81c44db06
cleytoncx <cleytoncicero2016@gmail.com>
2024-05-19 11:09:21 -0300

    adicionando informações iniciais sidebar e descrição inicial do projeto

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st 
 
-st.title("Meu primeiro titulo.")
+# configurações
 
+st.set_page_config(
+    page_title="Análista de Transporte",page_icon="🚚", layout="centered"
+)
+
+# SIDEBAR
+
+with st.sidebar:
+    st.image("imagens\Imagem do WhatsApp de 2024-05-19 à(s) 10.57.00_17aa87d2.jpg",width=100)
+    st.title("Integração - Analista de Transporte")
+    st.subheader('Cleyton Cicero')
+    st.divider()
+    st.write("Essa apresentação, visa apresentar um resumo do que já consegui aprender no meu processo de integração na Cardeal Distribuidora.")
+
+st.title("🚦Análista de Transporte")
+st.subheader("Cardeal Distribuídora")
+
+st.divider()
+
+st.subheader("Torre de Controle: Atendimento aos chamados")
+st.write("A torre de controle é responsável por monitorar e gerenciar todas as operações de transporte. O objetivo é garantir uma resposta rápida e eficiente para manter as operações em movimento e resolver problemas rapidamente.")
